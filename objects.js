ef754860caaa65dc4f071f006b75a5f59ec6cb98
Pearboy <jty2114@gmail.com>
2016-12-25 09:45:43 -0500

    Initial commit

---
+++ objects.js
@@ -0,0 +1,97 @@
+var squares = [];
+var interval = 2000;
+var level = 1;
+// square object
+function square(x, y, width, height, velX, velY) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.velX = velX;
+    this.velY = velY;
+    this.friction = 0.98;
+}
+
+function getRandom(max, min) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function newSquare() {
+    var newObject = new square(getRandom(canvasWidth, 0), 0, 30, 30, 1, 3);
+    squares.push(newObject);
+}
+
+function renderSquare(object) {
+    mainContext.beginPath();
+    mainContext.rect(object.x, object.y, object.width, object.height);
+    mainContext.fillStyle = "blue";
+    mainContext.fill();
+}
+
+function animateObject() {
+    var i;
+    for (i = 0; i < squares.length; i++) {
+        // Objects will start moving down to rain over the player
+        squares[i].y += squares[i].velY;
+        if (squares[i].velY < 3) {
+            squares[i].velY++;
+        }
+        squares[i].velY *= squares[i].friction;
+        checkObjectCanvasCollision(squares[i]);
+
+        // Player collided to an object (Game Over)
+        if (checkPlayerCollision(squares[i])) {
+            document.getElementById("status").innerHTML = "Game Over";
+            cancelAnimationFrame(animationId);
+            window.clearInterval(timer);
+            window.clearInterval(difficulty);
+        }
+        renderSquare(squares[i]);
+    }
+}
+
+function checkObjectCanvasCollision(object) {
+    if (object.y > canvasHeight - object.height || object.x < 0 
+            || object.x > canvasWidth - object.width) {
+        mainContext.clearRect(object.x, object.y, object.width, object.height);
+        var index = squares.indexOf(object);
+        squares.splice(index, 1)
+    }
+}
+
+function checkPlayerCollision(object) {
+    return !(player.x > object.x + object.width || player.x + player.width < object.x 
+            || player.y > object.y + object.height || player.y + player.height < object.y);
+}
+
+// Create a new square every 4 seconds
+// Initial square amount is 3
+var squareAmount = 3;
+window.setInterval(function() {
+    for (var j = 0; j < squareAmount; j++) {
+        newSquare();
+    }
+}, interval);
+
+// Difficulty increases as time increases
+var difficulty = window.setInterval(function() {
+    level++;
+    squareAmount++;
+    // Every five levels, faster square spawning
+    if (interval > 500 && level % 5 == 0) {
+        interval -= 500;
+    }
+    document.getElementById("level").innerHTML = "Level: " + level;
+}, 10000);
+
+// Record the time that the player survives
+var second = 0;
+var minute = 0;
+var timer = window.setInterval(function() {
+    second++;
+    if (second == 60) {
+        second = 0;
+        minute++;
+    }
+    document.getElementById("timer").innerHTML = ("0" + minute).slice(-2) + ":" + ("0" + second).slice(-2);
+}, 1000);
---
+++ player.js
@@ -0,0 +1,117 @@
+var player = {
+    x: 0,
+    y: 0,
+    width: 20,
+    height: 20,
+    maxspeed: 2,
+    friction: 0.90,
+    velX: 0,
+    velY: 0
+}
+var keys = [];
+var mainCanvas;
+var mainContext;
+var canvasWidth;
+var canvasHeight;
+var animationId;
+
+function init() {
+    mainCanvas = document.getElementById("container");
+    mainContext = mainCanvas.getContext("2d");
+    canvasWidth = mainCanvas.width;
+    canvasHeight = mainCanvas.height;
+    player.y = canvasHeight - player.height;
+    player.x = canvasWidth / 2;
+    mainCanvas.focus();
+    drawSpawnPoint();
+    animateObject();
+    playerAnimate();
+}
+
+// Spawn point of objects coming down
+function drawSpawnPoint() {
+    mainContext.rect(0, 0, canvasWidth, 100);
+    mainContext.fillStyle = "grey";
+    mainContext.fill();
+}
+
+function playerAnimate() {
+    animationId = requestAnimationFrame(playerAnimate);
+    // Display player coordinates
+    document.getElementById("coordinates").innerHTML = "x: " + player.x + " y: " + player.y;
+    mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawSpawnPoint();
+
+    // Handle key events
+    // Up
+    if (keys[38]) {
+        if (player.velY > -player.maxspeed) {
+            player.velY--;
+        }
+    }
+    // Down
+    if (keys[40]) {
+        if (player.velY < player.maxspeed) {
+            player.velY++;
+        }
+    }
+    // Right
+    if (keys[39]) {
+        if (player.velX < player.maxspeed) {
+            player.velX++;
+        }
+    }
+    // Left
+    if (keys[37]) {
+        if (player.velX > -player.maxspeed) {
+            player.velX--;
+        }
+    }
+    // Check if player has hit cavas boundary
+    checkPlayerCanvasCollision();
+
+    player.x += player.velX;
+    player.y += player.velY;
+
+    // Friction for smooth movement
+    player.velX *= player.friction;
+    player.velY *= player.friction;
+
+    player.x = Math.round(player.x);
+    player.y = Math.round(player.y);
+
+    // Redraw the player position
+    mainContext.beginPath();
+    mainContext.rect(player.x, player.y, player.width, player.height);
+    mainContext.fillStyle = "red";
+    mainContext.fill();
+    animateObject();
+}
+
+function checkPlayerCanvasCollision() {
+    if (player.y < 100) {
+        player.velY = 0;
+        player.y = 100;
+    }
+    if (player.y > canvasHeight - player.height) {
+        player.velY = 0;
+        player.y = canvasHeight - player.height;
+    }
+    if (player.x < 0) {
+        player.velX = 0;
+        player.x = 0;
+    }
+    if (player.x > canvasWidth - player.width) {
+        player.velX = 0;
+        player.x = canvasWidth - player.width;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+    keys[e.keyCode] = true;
+    e.preventDefault();
+});
+document.addEventListener("keyup", function (e) {
+    keys[e.keyCode] = false;
+    e.preventDefault();
+});
